fb1810c18efa18974931b8731cb5b4bd57898596
xXNanuXx <nanu1429@gmail.com>
2023-10-02 02:02:53 -0300

    correccion de merge

--- BACKEND/Controllers/userController.js
+++ BACKEND/Controllers/userController.js
@@ -8,6 +8,21 @@ class userController {
             res.send({ success: true, message: "Usuarios encontrados", data });
         })
     }
+    createUser = (req,res,next) => {
+        const { nombre, apellido, correo, contrasena } = req.body;
+        const newUser = {
+            nombre : nombre,
+            apellido : apellido,
+            correo : correo,
+            contrasena : contrasena
+        };
+        dataBase.ref('usuarios').push(newUser);
+        res.send("Usuario creado con exito")
+    }
+    deleteUser = (req, res, next) => {
+        console.log(req.params.id)
+        dataBase.ref('usuarios/' + req.params.id).remove()
+    }
 }
 
 export default userController
--- BACKEND/Routes/userRoutes.js
+++ BACKEND/Routes/userRoutes.js
@@ -1,5 +1,4 @@
 import { Router } from "express";
-import dataBase from '../ConectionDb/connectionDb.js'
 import UserController from "../Controllers/userController.js";
 
 const userController = new UserController(); 
@@ -7,21 +6,7 @@ const userController = new UserController();
 const userRoutes = Router()
 
 userRoutes.get("/", userController.getAllUsers)
-
-userRoutes.post("/new-user", (req,res) => {
-    const { nombre, apellido, correo, contrasena } = req.body;
-    const newUser = {
-        nombre : nombre,
-        apellido : apellido,
-        correo : correo,
-        contrasena : contrasena
-    };
-    dataBase.ref('usuarios').push(newUser);
-    res.send("Usuario creado con exito")
-})
-
-userRoutes.get("/delete-user", (req, res) => {
-    dataBase.ref('usuarios/' + req.params.id).remove
-})
+userRoutes.post("/new-user", userController.createUser)
+userRoutes.delete("/delete-user/:id", userController.deleteUser)
 
 export default userRoutes
