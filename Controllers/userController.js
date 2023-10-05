42d1923e66f394a35e02bee6227c65ea9d9f16b5
xXNanuXx <nanu1429@gmail.com>
2023-10-04 21:55:36 -0300

    Login base terminado

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -10,50 +10,66 @@ class userController {
     }
 
     createUser = (req,res,next) => {
-        const { nombre, apellido, correo, contrasena, perfil } = req.body;
-        const newUser = {
-            nombre : nombre,
-            apellido : apellido,
-            correo : correo,
-            contrasena : contrasena,
-            perfil : perfil
-        };
-        dataBase.ref('usuarios').push(newUser);
-        res.send("Usuario creado con exito")
+        try{
+            const { nombre, apellido, correo, contrasena, perfil } = req.body;
+            const newUser = {
+                nombre : nombre,
+                apellido : apellido,
+                correo : correo,
+                contrasena : contrasena,
+                perfil : perfil
+            };
+            dataBase.ref('usuarios').push(newUser);
+            res.status(200).send("Usuario creado con exito")
+        } catch{
+            res.status(404).send({ success: false, result: "Usuario no registrado" });
+        }
+        
     }
 
     login = async (req, res, next) => {
-        const { correo, contrasena } = req.body;
-        console.log(correo)
+
         try {
+            const { correo, contrasena } = req.body;
+            
             const snapshot = await dataBase.ref('usuarios')
                                       .orderByChild('correo')
                                       .equalTo(correo)
-                                      .once('value')
-        
-            //if (!snapshot.exists()) {
-            //    res.status(404).send({ success: false, result: "Correo no registrado" });
-            //}
+                                      .once("value")
+            
+            if (!snapshot.exists()) {
+                throw new Error("Correo no registrado");
+            }
 
-            //const usuarios = [];
-//
-            //snapshot.forEach((childSnapshot) => {
-            //  usuarios.push({
-            //    id: childSnapshot.key,
-            //    ...childSnapshot.val()
-            //  });
-            //});
+            let usuarioValido = false;
 
-            console.log(usuarios)
-        
-          } catch (error) {
-            res.status(400).send({ success: false, result: error.message });
-          }
+            snapshot.forEach((childSnapshot) => {
+              const usuario = childSnapshot.val();
+              if (usuario.contrasena === contrasena) {
+                usuarioValido = true;
+              }
+            });
+
+            if (!usuarioValido) {
+                throw new Error("Contraseña incorrecta");
+            }
+            
+            res.status(200).send({ success: true, message: "Inicio de sesión exitoso" });
+            
+        } catch{
+            console.error("Error al validar usuario:", message.error);
+            res.status(404).send({ success: false, result: error.message });
+        }
     }
       
     deleteUser = (req, res, next) => {
-        console.log(req.params.id)
-        dataBase.ref('usuarios/' + req.params.id).remove()
+        try {
+            dataBase.ref('usuarios/' + req.params.id).remove()
+            res.status(200).send({ success: true, message: "Usuario borrado" });
+        } catch {
+            res.status(400).send({ success: false, result: error.message });
+        }
+        
     }
 }
 
